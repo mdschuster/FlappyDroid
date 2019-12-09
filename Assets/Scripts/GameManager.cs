36c20a27786bce810a609ade53af6d5e3faf5a9b
Micah Schuster <schusterm@wit.edu>
2019-12-09 11:20:36 -0500

    Add pipe spawn behavior and pipe movement

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -4,15 +4,42 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
+
+    [SerializeField]
+    private GameObject pipePrefab;
+
+    [SerializeField]
+    private float spawnRange=3f;
+
+    [SerializeField]
+    private float timeBetweenSpawn = 3;
+    private float spawnCooldown;
+    private float spawnTime=0;
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(pipePrefab==null) Debug.LogError("PipePrefab not found!");
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(spawnTime<=0){
+            spawnTime=timeBetweenSpawn;
+            //random y
+            float ySpawn=Random.Range(-spawnRange,spawnRange);
+            float xSpawn=10f;
+            //actually do the spawn
+            Instantiate(pipePrefab,new Vector3(xSpawn,ySpawn,0f), Quaternion.identity);
+
+        } else {
+            spawnTime-=Time.deltaTime;
+        }
+    }
+
+    private void spawnPipe(){
+
     }
 }
---
+++ Assets/Scripts/Pipe.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Pipe : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void FixedUpdate() {
+        //pipe object should move in one direction at a constant speed
+        //TODO: Should probably use translate here
+        Vector3 pos = this.transform.position;
+        pos.x-=0.1f;
+        this.transform.position=pos;
+    }
+}
